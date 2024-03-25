4a75d4e5f10bc0686f035c8a29eb0455571d2e7b
w8be <60948440+w8be@users.noreply.github.com>
2024-03-25 09:35:27 -0400

    update

---
+++ Flex USB PTT files/flexRadio-usb-ptt.ino
@@ -0,0 +1,37 @@
+#include <Bounce2.h>
+#define PTT 2
+int PTT_State = 0;
+
+Bounce debouncer = Bounce();
+
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    pinMode(PTT, INPUT_PULLUP);
+    debouncer.attach(PTT);
+    debouncer.interval(200); // interval in ms
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    debouncer.update();
+    // Serial.println("ZZAI;");
+    if (debouncer.read() != PTT_State)
+    {
+        if (digitalRead(PTT) == LOW)
+        {
+            delay(100);
+            Serial.println("ZZTX1;");
+            PTT_State = 0;
+        }
+        else
+        {
+            delay(100);
+            Serial.println("ZZTX0;");
+            PTT_State = 1;
+            Serial.println("ZZTX0;");
+        } // just in case radio missing it
+    }
+}
